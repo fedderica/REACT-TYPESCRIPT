1eb8306309ecaa58b8b8b87773b013fb5f1bd4b4
fedderica <fedderica.bartoli@gmail.com>
2023-03-28 10:28:32 -0300

    imagenes+stylos

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,39 @@ import './App.css';
 
 
 function App() {
-  const [number, setNumber] = useState<number | string>(5)
+  const [subs, setSubs] = useState([
+{
+  nick: 'dapelu',
+  subMonths: 3,
+  avatar: 'https://i.pravatar.css/150?=sergio_serrano',
+  description: 'Dapelu hace de moderador a veces'
+},
+{
+  nick: 'sergio_serrano',
+  subMonths: 7,
+  avatar: 'https://i.pravatar.cc/150?u=sergio_serrano'
+}
+])
 
-  const changeNumber = () => {
-    setNumber("2")
-    }
+
+
 
   return (
     <div className="App">
-      {number}
-      <button onClick={changeNumber}>Change number</button>
+     <h1>midu subs</h1>
+     <ul>
+      {
+        subs.map(sub => {
+          return (
+            <li key={sub.nick}>
+              <img src={sub.avatar} alt={`Avatar for ${sub.nick}`} />
+              <h4>{sub.nick} (<small>{sub.subMonths}</small>)</h4>
+           <p>{sub.description?.substring(0, 100)}</p> 
+           </li>
+          )
+        })
+      }
+     </ul>
     </div>
   );
 }
