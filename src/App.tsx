146125aaaf5a5827715aee9f4e43089e9f6724b5
fedderica <fedderica.bartoli@gmail.com>
2023-03-28 09:54:40 -0300

    BASE-REACT-TYPESCRIPT

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+import './App.css';
+
+
+
+function App() {
+  const [number, setNumber] = useState<number | string>(5)
+
+  const changeNumber = () => {
+    setNumber("2")
+    }
+
+  return (
+    <div className="App">
+      {number}
+      <button onClick={changeNumber}>Change number</button>
+    </div>
+  );
+}
+
+export default App;
